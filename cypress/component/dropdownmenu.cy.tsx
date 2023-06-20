0dce3755f7dc02e1a420ff6138e4c59c38b203f3
mauricio dall onder <londer11@icloud.com>
2023-06-20 20:36:44 -0300

    feat: creating component tests with cypress

---
+++ cypress/component/dropdownmenu.cy.tsx
@@ -0,0 +1,40 @@
+import { DropDownMenu } from "@/components/DropDownFilter";
+import { store } from "@/redux/Store";
+import { mount } from "cypress/react18";
+import { Provider } from "react-redux";
+
+
+describe("DropDownMenu Component", () => {
+    beforeEach(() => {
+        mount(
+            <Provider store={store}>
+                <DropDownMenu />
+            </Provider>
+        );
+    });
+
+    it("verifica se o menu abre e fecha ao clicar no botão", () => {
+        cy.get("button").click();
+        cy.get('[data-testid="priority-filter"]').should("be.visible");
+
+        cy.get("button").click();
+        cy.get('[data-testid="priority-filter"]').should("not.exist");
+    });
+
+    it("verifica a seleção dos diferentes métodos de classificação", () => {
+        const options = [
+            { id: "sort-by-default", label: "" },
+            { id: "sort-by-novidades", label: "Novidades" },
+            { id: "sort-by-preco-maior", label: "Preço: Maior - menor" },
+            { id: "sort-by-preco-menor", label: "Preço: Menor - maior" },
+            { id: "sort-by-mais-vendidos", label: "Mais vendidos" },
+        ];
+
+        options.forEach((option) => {
+            cy.get("button").click();
+            cy.get(`[data-testid="${option.id}"]`).click();
+            cy.contains(option.label || "Organizar por");
+        });
+    });
+});
+
---
+++ cypress/component/filterbar.cy.tsx
@@ -0,0 +1,34 @@
+
+import { FilterBar } from '@/components/FilterBar'
+import { store } from '@/redux/Store'
+import { mount } from 'cypress/react18'
+import { Provider } from 'react-redux'
+
+
+describe('FilterBar Component', () => {
+  beforeEach(() => {
+    mount(
+      <Provider store={store}>
+        <FilterBar />
+      </Provider>,
+    )
+  })
+
+  it('Verifica a seleção de todos os produtos', () => {
+    cy.get('button').contains('todos os produtos').click()
+      // Verifica se a categoria todos os produtos corretamente foi definida
+    cy.get('[data-category="todos os produtos"]').should('exist')
+  })
+
+  it('Verifica a seleção de camisetas', () => {
+    cy.get('button').contains('camisetas').click()
+    // Verifica se a categoria camisetas corretamente foi definida
+    cy.get('[data-category="camisetas"]').should('exist')
+  })
+
+  it('Verifica a seleção de canecas', () => {
+    cy.get('button').contains('canecas').click()
+    // Verifica se a categoria canecas corretamente foi definida
+    cy.get('[data-category="canecas"]').should('exist')
+  })
+})
